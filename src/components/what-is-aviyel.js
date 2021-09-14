532b0a8da9bba715169a8a681fed9fe1585c9dcb
Hadeeb Farhan <z10@aviyel.com>
2021-09-15 00:21:50 +0530

    Add what is aviyel

---
+++ src/components/what-is-aviyel.js
@@ -0,0 +1,11 @@
+import { logos } from "../images/common";
+
+function WhatIsAviyel() {
+  return (
+    <mj-column background-color="#e7fcf3" padding="30px">
+      <mj-image src={logos.whatIsAviyel} />
+    </mj-column>
+  );
+}
+
+export { WhatIsAviyel };
--- src/images/common.js
+++ src/images/common.js
@@ -3,6 +3,8 @@ const logos = {
   calendarOrange:
     "https://beta.aviyel.com/assets/uploads/static/icon-calendar.png",
   duration: "https://beta.aviyel.com/assets/uploads/static/icon-duration.png",
+  whatIsAviyel:
+    "https://beta.aviyel.com/assets/uploads/static/what-is-aviyel.png",
 };
 
 export { logos };
