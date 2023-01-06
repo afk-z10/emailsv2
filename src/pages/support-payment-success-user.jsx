cd397b96bcd1a8247e5e821d10ebae4aa30f6b8b
Hadeeb Farhan <z10@aviyel.com>
2023-01-06 17:43:45 +0530

    update user payment success template

--- src/pages/support-payment-success-user.jsx
+++ src/pages/support-payment-success-user.jsx
@@ -1,6 +1,6 @@
 import colors from "../common/colors";
 import { font } from "../common/styles";
-import { CommunityHeader } from "../components/community/header";
+import { logos } from "../images/common";
 import { useData } from "../utils";
 
 function SupportPaymentSuccessUser() {
@@ -17,25 +17,52 @@ function SupportPaymentSuccessUser() {
       <mj-body background-color="#F6F6F9">
         <mj-section padding="32px" background-color={colors.white}>
           <mj-section padding="8px 0px">
-            <CommunityHeader />
+            <mj-table>
+              <tr>
+                <td>
+                  <img
+                    align="left"
+                    src={useData(
+                      "project_logo",
+                      "https://placeimg.com/64/64/tech"
+                    )}
+                    height="64px"
+                    width="64px"
+                  />
+                </td>
+                <td>
+                  <img
+                    align="right"
+                    src={logos.aviyel_small}
+                    height="40px"
+                    width="40px"
+                  />
+                </td>
+              </tr>
+            </mj-table>
           </mj-section>
 
           <mj-spacer height="16px" />
-          <mj-text font-size="18px" font-weight="600" color={colors.grey[1]}>
-            Payment Success
+          <mj-text font-size="38px" font-weight="700" color={colors.grey[1]}>
+            Payment Success!
           </mj-text>
 
-          <mj-spacer height="16px" />
-          <mj-text font-size="16px" font-weight="600" color={colors.grey[1]}>
-            Some more title stuff
+          <mj-spacer height="12px" />
+          <mj-text font-size="22px" font-weight="600" color={colors.grey[1]}>
+            Thank you for opting for Priority Support!
           </mj-text>
 
-          <mj-spacer height="16px" />
-          <mj-text font-size="16px" color={colors.grey[2]}>
-            Your contributions are valued, ... ...
+          <mj-spacer height="40px" />
+          <mj-text font-size="18px" color={colors.grey[2]}>
+            Hey {useData("name", "John")},
+            <div style={`height:16px`} />
+            Your payment has been successfully processed and you will now have
+            access to expedited support. Our team will do their best to get you
+            quicker responses for your issues/questions from the maintainer team
+            of {useData("", "Typesense")} as quickly as possible.
           </mj-text>
 
-          <mj-spacer height="16px" />
+          <mj-spacer height="32px" />
           <mj-button
             font-size="14px"
             font-weight="600"
@@ -43,14 +70,23 @@ function SupportPaymentSuccessUser() {
             padding="8px 20px"
             background-color={colors.primary.main}
             href={useData(
-              "slack_link",
+              "slack_url",
               "https://cosmicvillage.slack.com/archives/C044Y487J9M/p1672730674328179"
             )}
             target="_blank"
             rel="noopener noreferrer"
           >
-            View on Slack
+            Check Your Thread on Slack
           </mj-button>
+
+          <mj-spacer height="32px" />
+          <mj-text font-size="16px" color={colors.grey[2]}>
+            Thank you for choosing our service.
+            <div style={`height:16px`} />
+            Cheers,
+            <br />
+            <span style={`font-weight:600;`}>Team Aviyel</span>
+          </mj-text>
         </mj-section>
       </mj-body>
     </mjml>
